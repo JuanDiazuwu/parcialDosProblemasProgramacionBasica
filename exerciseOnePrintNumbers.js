f92b1944a33eec0bd3c44afcb3d962bdf5f6054f
JUAN ANTONIO DIAZ FERNANDEZ <a348637@uach.mx>
2023-03-28 18:40:42 -0600

    1.- Hacer un programa que imprima los números del 1 al 100

---
+++ exerciseOnePrintNumbers.js
@@ -0,0 +1,14 @@
+/*
+function printNumbers(){
+    for(let i = 1; i < 101; i++){
+        console.log(i)
+    }
+};*/
+
+const printNumbers = () => {
+    for(let i = 1; i < 101; i++){
+        console.log(i)
+    }
+};
+
+printNumbers();
