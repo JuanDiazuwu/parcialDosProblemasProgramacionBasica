d36ac141bf10b2b05fc3ea8a667c59223b0e5bba
JUAN ANTONIO DIAZ FERNANDEZ <a348637@uach.mx>
2023-04-02 12:20:25 -0600

    28.- Simular el lanzamiento de una moneda al aire e imprimir cara o cruz.

---
+++ exerciseTwentyEightThrowCoin.js
@@ -0,0 +1,13 @@
+// 28.- Simular el lanzamiento de una moneda al aire e imprimir cara o cruz. 
+
+const throwCoin = () =>{
+    let uwu = Math.round(Math.random());
+    if(uwu == 0){
+        console.log('Cara!!!');
+    } else {
+        console.log('Cruz!!!');
+    }
+}
+
+throwCoin();
+
